f43e9d9234981cc42d206600e50df3ea2c7f4ba9
John M <mahan.john@gmail.com>
2022-02-28 03:04:03 -0800

    deploy

---
+++ src/Components/Portfolio.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import Zmage from "react-zmage";
+import Fade from "react-reveal";
+import { Button } from "react-bootstrap";
+
+let id = 0;
+class Portfolio extends Component {
+  render() {
+    if (!this.props.data) return null;
+
+    const projects = this.props.data.projects.map(function (projects) {
+      let projectImage = "images/portfolio/" + projects.image;
+
+      return (
+        <div key={id++} className="columns portfolio-item">
+          <div className="item-wrap">
+            <Zmage alt={projects.title} src={projectImage} />
+            <div style={{ textAlign: "center" }}>{projects.title}</div>
+            <Button href={projects.url}>view demo</Button>
+            <Button href={projects.source}>view source</Button>
+          </div>
+        </div>
+      );
+    });
+
+    return (
+      <section id="portfolio">
+        <Fade left duration={1000} distance="40px">
+          <div className="row">
+            <div className="twelve columns collapsed">
+              <h1>Check Out Some of My Work</h1>
+
+              <div
+                id="portfolio-wrapper"
+                className="bgrid-quarters s-bgrid-thirds cf"
+              >
+                {projects}
+              </div>
+            </div>
+          </div>
+        </Fade>
+      </section>
+    );
+  }
+}
+
+export default Portfolio;
